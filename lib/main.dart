bed997b36ee4014bb405ee2481ade5eaea875587
sedasenbol <ssenbol@gmail.com>
2023-05-13 22:34:27 +0300

    Some style changes.

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,19 @@ void main() {
           body: Center(
               child: Column(mainAxisSize: MainAxisSize.min, children: [
             Image.asset('assets/images/quiz-logo.png', width: 300),
-            const SizedBox(
-              height: 20
-            ),
+            const SizedBox(height: 20),
             const Text("Learn Flutter the fun way!",
-                selectionColor: Colors.white),
-                const SizedBox(
-              height: 20
-            ),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                )),
+            const SizedBox(height: 20),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                  textStyle: const TextStyle(fontSize: 20)),
               child: const Text('Start Quiz'),
-              onPressed: () {
-                
-              },
+              onPressed: () {},
             ),
           ]))),
     ),
